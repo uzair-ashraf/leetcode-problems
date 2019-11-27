0f49723f87df4b21e2f4b3b918d911931e0fa59d
Uzair Ashraf <uzinatorcl@live.com>
2019-11-27 12:06:39 -0800

    fixed two sum issue with accumulator

--- main.js
+++ main.js
@@ -4,22 +4,26 @@
 //You may assume that each input would have exactly one solution, and you may not use the same element twice.
 //Example Input = [2, 7, 11, 15], 9
 //Example Output = [0,1]
-
 var twoSum = function (nums, target) {
   var firstIndex = null;
   var secondIndex = null;
+
   for (var i = 0; i < nums.length; i++) {
-    nums.reduce((acc, currentValue, index) => {
-      if (acc + currentValue === target) {
-        debugger;
+    nums.forEach((number, index) => {
+      if (number + nums[i] === target && i !== index) {
         firstIndex = index;
         secondIndex = i;
       }
-    }, nums[i])
+    })
     if (firstIndex !== null && secondIndex !== null) {
       return [firstIndex, secondIndex]
     }
   }
+  nums.forEach((number, index) => {
+    if (number + number === target) {
+      return [index, index]
+    }
+  })
 };
 
-twoSum([2,7,11,15], 9);
+twoSum([3,2,4], 6);
